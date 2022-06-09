9fd70edc5e579a4d3f6c9b2aa7e6d9e359520377
David Braley <davidbraley@khanacademy.org>
2022-06-08 23:16:49 -0400

    1.0 - 1.5 (take 2)

---
+++ rust/rust-in-action/hello/target/debug/deps/hello-c7adf135f0e0c2b0.d
@@ -0,0 +1,5 @@
+/Users/davidbraley/git/dbraley/tilde/rust/rust-in-action/hello/target/debug/deps/hello-c7adf135f0e0c2b0: src/main.rs
+
+/Users/davidbraley/git/dbraley/tilde/rust/rust-in-action/hello/target/debug/deps/hello-c7adf135f0e0c2b0.d: src/main.rs
+
+src/main.rs:
---
+++ rust/rust-in-action/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/davidbraley/git/dbraley/tilde/rust/rust-in-action/hello/target/debug/hello: /Users/davidbraley/git/dbraley/tilde/rust/rust-in-action/hello/src/main.rs
