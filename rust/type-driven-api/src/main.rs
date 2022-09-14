02386600a41dc64a0e349f161238a2c0a93c57ed
David Braley <davidbraley@khanacademy.org>
2022-09-14 00:51:30 -0400

    Implement Type State

--- rust/type-driven-api/src/main.rs
+++ rust/type-driven-api/src/main.rs
@@ -2,40 +2,73 @@ use std::{time::Duration, thread::sleep};
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
-struct Progress<Iter> {
-    iter: Iter,
-    i: usize,
-    bound: Option<usize>
+struct Unbounded;
+struct Bounded {
+    bound: usize,
+    delims: (char, char)
 }
 
-impl<Iter> Progress<Iter> {
-    pub fn new(iter: Iter) -> Self {
-        Progress{ iter, i:0, bound: None }
+struct Progress<Iter, Bound> {
+    iter: Iter,
+    i: usize,
+    bound: Bound
+}
+
+trait ProgressDisplay: Sized {
+    fn display<Iter>(&self, progress: &Progress<Iter, Self>);
+}
+
+impl ProgressDisplay for Unbounded {
+    fn display<Iter>(&self, progress: &Progress<Iter, Self>) {
+        println!("{}", "*".repeat(progress.i))
     }
 }
 
-impl<Iter> Progress<Iter>
+impl ProgressDisplay for Bounded {
+    fn display<Iter>(&self, progress: &Progress<Iter, Self>) {
+        print!("{}{}{}{}",
+               self.delims.0,
+               "*".repeat(progress.i),
+               " ".repeat(self.bound - progress.i),
+               self.delims.1);
+    }
+}
+
+impl<Iter> Progress<Iter, Unbounded> {
+    pub fn new(iter: Iter) -> Self {
+        Progress{ iter, i:0, bound: Unbounded}
+    }
+}
+
+impl<Iter> Progress<Iter, Unbounded>
 where Iter: ExactSizeIterator {
-    pub fn with_bound(mut self) -> Self {
-        self.bound = Some(self.iter.len());
+    pub fn with_bound(self) -> Progress<Iter, Bounded> {
+        let bound = Bounded {
+            bound: self.iter.len(),
+            delims: ('[', ']')
+        };
+        Progress {
+            i: self.i,
+            iter: self.iter,
+            bound
+        }
+    }
+}
+
+impl<Iter> Progress<Iter, Bounded> {
+    pub fn with_delims(mut self, delims: (char, char)) -> Self {
+        self.bound.delims = delims;
         self
     }
 }
 
-impl<Iter> Iterator for Progress<Iter>
-where Iter: Iterator {
+impl<Iter, Bound> Iterator for Progress<Iter, Bound>
+where Iter: Iterator, Bound: ProgressDisplay {
     type Item = Iter::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
         print!("{}", CLEAR);
-        match self.bound {
-            Some(bound) =>
-                print!("[{}{}]",
-                    "*".repeat(self.i),
-                    " ".repeat(bound - self.i)),
-            None =>
-                println!("{}", "*".repeat(self.i))
-        }
+        self.bound.display(&self);
         self.i += 1;
 
         self.iter.next()
@@ -43,11 +76,11 @@ where Iter: Iterator {
 }
 
 trait ProgressIterExt: Sized {
-    fn progress(self) -> Progress<Self>;
+    fn progress(self) -> Progress<Self, Unbounded>;
 }
 
 impl <Iter> ProgressIterExt for Iter {
-    fn progress(self) -> Progress<Self> {
+    fn progress(self) -> Progress<Self, Unbounded> {
         Progress::new(self)
     }
 }
@@ -57,8 +90,9 @@ fn expensive_calculation(_n: &i32) {
 }
 
 fn main() {
+    let delims = ('<', '>');
     let v = vec![1,2,3];
-    for n in v.iter().progress().with_bound() {
+    for n in v.iter().progress().with_bound().with_delims(delims) {
         expensive_calculation(n)
     }
 }
